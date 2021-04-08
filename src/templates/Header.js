890eb5b34bf7c4b3b8f76dfe402f85a0c58a638a
Joseph Salen <jsalen.91@gmail.com>
2021-04-08 00:12:48 -0400

    Created Header template and Character page

---
+++ src/templates/Header.js
@@ -0,0 +1,20 @@
+const Header = () => {
+  const view = `
+        <div class="header-main">
+            <div class="header-logo>
+                <h1>
+                    <a href="/">Single Page App</a>
+                </h1>
+            </div>
+            <div class="header-nav>
+                <a href="#/about/">
+                    About
+                </a>
+            </div>
+        </div>
+    `;
+
+  return view;
+};
+
+export default Header;
